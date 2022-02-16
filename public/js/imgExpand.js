f256cbb02bbdd602a0570416031e1417bf4bb882
craM-Hub <anarka84@gmail.com>
2022-02-16 09:49:30 +0100

    misjquery, scrolltop

--- public/js/imgExpand.js
+++ public/js/imgExpand.js
@@ -1,7 +1,7 @@
 // Modal Image Gallery
 function imgExpand(element) {
-    document.getElementById('img01').src = element.src;
-    document.getElementById('modal01').style.display = 'block';
-    var captionText = document.getElementById('caption');
-    captionText.innerHTML = element.alt;
+	document.getElementById('img01').src = element.src;
+	document.getElementById('modal01').style.display = 'block';
+	var captionText = document.getElementById('caption');
+	captionText.innerHTML = element.alt;
 }
--- public/js/misjquery.js
+++ public/js/misjquery.js
@@ -1,4 +1,5 @@
 $(function () {
+	// Formulario envio fotos, muestra foto seleccionada
 	$('#photo_fileName').on('change', function (event) {
 		$('#fileName-container').show();
 
@@ -6,5 +7,11 @@ $(function () {
 		$('#fileName-container').html(
 			'Foto seleccionada: ' + inputFile.value.substr(12)
 		);
+	});
+
+	// Scroll top
+	$("a[href='#top']").click(function () {
+		$('html, body').animate({ scrollTop: 0 }, 'slow');
+		return false;
 	});
 });
