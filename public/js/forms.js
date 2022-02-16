82721562a54256a8251014f523522b17a27dfa6a
craM-Hub <anarka84@gmail.com>
2022-02-16 09:01:31 +0100

    fix forms.js

--- public/js/forms.js
+++ public/js/forms.js
@@ -1,8 +1,7 @@
 $(function () {
 	$('#photo_fileName').on('change', function (event) {
 		$('#fileName-container').show();
-	});
-	$('#photo_fileName').on('change', function (event) {
+
 		let inputFile = event.currentTarget;
 		$('#fileName-container').html(
 			'Foto seleccionada: ' + inputFile.value.substr(12)
